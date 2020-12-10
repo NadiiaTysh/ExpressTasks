0512af8029564d52d4c91dded3337a362477998d
Nadia.Auto <nadiia.tyshko@fortegrp.com>
2020-12-10 19:30:15 +0200

    lesson-11-task-4

--- source/utils/validator.js
+++ source/utils/validator.js
@@ -1,17 +1,33 @@
 // Core
 import Ajv from 'ajv';
+import { ValidationError } from './errors';
 
 export const validator = (schema) => (req, res, next) => {
     const ajv = new Ajv({allErrors: true});
     const validate = ajv.compile(schema);
-
     const isValid = validate(req.body);
 
     if (isValid) {
         return next();
     }
 
-    const errors = validate.errors.map(({ message }) => message).join(', ');
+    const errors = validate.errors.map(({ params, dataPath }) => {
+        const { additionalProperty, missingProperty } = params;
+        let error = '';
 
-    res.status(400).json({ message: errors });
+        if (additionalProperty) {
+            error = additionalProperty;
+        }
+        if (missingProperty) {
+            error = missingProperty;
+        }
+        if (dataPath) {
+            error = dataPath.slice(1);
+        }
+
+        return error.concat(' error');
+    });
+
+    const body = JSON.stringify(req.body, null, 2);
+    next(new ValidationError(`${req.method}: ${req.originalUrl} [ ${errors.join(', ')} ]\n${body}`, 400));
 };
