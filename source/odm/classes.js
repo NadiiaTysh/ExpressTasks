116c60eded93b8ffb0a79505e56dbb59535de609
Nadia.Auto <nadiia.tyshko@fortegrp.com>
2021-02-22 20:10:50 +0200

    lesson-23-task-2

--- source/odm/classes.js
+++ source/odm/classes.js
@@ -69,4 +69,49 @@ classSchema.index({ title: 'text', description: 'text' });
 classSchema.index({ order: 1 });
 
 const classes = mongoose.model('classes', classSchema);
+
+classSchema.path('students').validate(async function(value) {
+    const addedUsersInClass = [];
+    value.every((userObject) => addedUsersInClass.push(userObject.user));
+
+    const data = await users.find({});
+    const existingUsers = [];
+    data.every((userInDb) => existingUsers.push(userInDb._id));
+
+    const result = addedUsersInClass.every((personAddedInClass) => {
+        const isSome = existingUsers.some((personExisted) => {
+            return personAddedInClass.toString() === personExisted.toString();
+        });
+        if (!isSome) {
+            throw new Error(`${personAddedInClass} is not found in 'students'`);
+        }
+
+        return isSome;
+    });
+
+    return result;
+});
+
+classSchema.path('lessons').validate(async function(value) {
+    const addedLessonsInClass = [];
+    value.every((lessonObject) => addedLessonsInClass.push(lessonObject.lesson));
+
+    const data = await lessons.find({});
+    const existingLessons = [];
+    data.every((lessonInDb) => existingLessons.push(lessonInDb._id));
+
+    const result = addedLessonsInClass.every((lessonAddedInClass) => {
+        const isSome = existingLessons.some((lessonExisted) => {
+            return lessonAddedInClass.toString() === lessonExisted.toString();
+        });
+        if (!isSome) {
+            throw new Error(`${lessonAddedInClass} is not found in 'lessons'`);
+        }
+
+        return isSome;
+    });
+
+    return result;
+});
+
 export { classes };
